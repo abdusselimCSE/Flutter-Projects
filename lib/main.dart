7dee55c67c0b5252af748dfab48f289b607d4d30
abdusselim <abdusselim999@gmail.com>
2024-12-10 23:24:12 +0600

    Google Map

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,18 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:sum_app/firebase_options.dart';
-import 'package:sum_app/live_score_screen.dart';
+import 'package:sum_app/map_screen.dart';
 
-Future<void>main()async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(LiveScoreApp());
-
+void main() {
+  runApp(const LocationTrackingApp());
 }
 
-class LiveScoreApp extends StatelessWidget {
-  const LiveScoreApp({super.key});
+class LocationTrackingApp extends StatelessWidget {
+  const LocationTrackingApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      home: LiveScoreScreen(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MapScreen(),
     );
   }
 }
