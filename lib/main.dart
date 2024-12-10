5f359b057a54ae6e34ee811342f4d88050bd288c
abdusselim <abdusselim999@gmail.com>
2024-12-10 22:54:55 +0600

    FireBase Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sum_app/firebase_options.dart';
+import 'package:sum_app/live_score_screen.dart';
 
-void main(){
-  runApp(MyApp());
+Future<void>main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(LiveScoreApp());
+
+}
+
+class LiveScoreApp extends StatelessWidget {
+  const LiveScoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: LiveScoreScreen(),
+    );
+  }
 }
